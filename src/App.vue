155007c0cf82c5d2e76f2fdda968fd168d2c1272
Felipe Carmona <felipecarmonamaturana@gmail.com>
2024-07-06 13:14:36 -0400

    tienda 32bits

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Tienda 32 bits</h1>
+  <h3>Lista de Juegos</h3>
+  <table border="1px">
+    <thead>
+      <th>Codigo</th>
+      <th>Nombre</th>
+      <th>Stock</th>
+      <th>Precio</th>
+      <th>Acciones</th>
+    </thead>
+    <tbody>
+      <tr v-for="(juego, i) in juegos" :key="i">
+        <td>{{ juego.codigo }}</td>
+        <td>{{ juego.nombre }}</td>
+        <td>{{ juego.stock }}</td>
+        <td>${{ Number(juego.precio).toLocaleString() }}</td>
+        <td>
+          <button @click="incrementar(i)">+</button>
+          <button @click="decrementar(i)">-</button>
+        </td>
+      </tr>
+    </tbody>
+
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState(["juegos"])
+  },
+  methods: {
+    ...mapActions(["incrementar", "decrementar"])    
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,26 @@
 import { createStore } from 'vuex'
+import juegos from "./juegos.json"
 
 export default createStore({
   state: {
+    juegos
   },
   getters: {
   },
   mutations: {
+    setJuegos(state, juegos) {
+      state.juegos = juegos
+    }
   },
   actions: {
+    incrementar(context, i) {
+      context.state.juegos[i].stock = context.state.juegos[i].stock + 1
+      context.commit("setJuegos", context.state.juegos)
+    },
+    decrementar(context, i) {
+      context.state.juegos[i].stock = context.state.juegos[i].stock - 1
+      context.commit("setJuegos", context.state.juegos)
+    }
   },
   modules: {
   }
